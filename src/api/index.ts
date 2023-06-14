24b6952572c224a732ea8bbc87d6c8de15c6b69b
sean <seandacquel@gmail.com>
2023-06-14 21:49:49 +0800

    [feat]: add axios cache and getDetails api wrapper

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,21 @@
-import axios from 'axios';
+import Axios from 'axios';
+import { setupCache } from 'axios-cache-interceptor';
 
-export const getAllPokemonList = async () => axios.get('https://pokeapi.co/api/v2/pokemon/')
+import { IPokeApi, IPokemonDetails } from '@/utils/types';
+
+const axios = setupCache(Axios);
+
+export const getPokemonList = async () => axios.get('https://pokeapi.co/api/v2/pokemon/')
   .then((response) => response.data)
   .catch((err) => Promise.reject(err));
 
-export const getPokemonDetails = async (id: string) => axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+export const getPokemonDetails = async (url: string) => axios.get(url)
   .then((response) => response.data)
   .catch((err) => Promise.reject(err));
+
+export const getPokemonDetailsFromList = async (results: IPokeApi[]) => {
+  const detailsUrlList = results.map((data) => getPokemonDetails(data.url));
+
+  return Promise.all(detailsUrlList)
+    .then(Axios.spread((...res) => res as IPokemonDetails[]));
+};
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,59 @@
-interface IPokemonDetails {
-    
+export interface IPokeApi {
+  name: string;
+  url: string;
+}
+
+export enum PokemonType {
+  normal = 'normal',
+  fire = 'fire',
+  water = 'water',
+  grass = 'grass',
+  electric = 'electric',
+  ice = 'ice',
+  fighting = 'fighting',
+  poison = 'poison',
+  ground = 'ground',
+  flying = 'flying',
+  psychic = 'psychic',
+  bug = 'bug',
+  rock = 'rock',
+  ghost = 'ghost',
+  dark = 'dark',
+  dragon = 'dragon',
+  steel = 'steel',
+  fairy = 'fairy',
+}
+
+interface IType extends IPokeApi {
+  name: PokemonType;
+}
+
+interface IPokemonType {
+  type: IType;
+  slot: number;
+}
+
+interface IFront {
+  front_default: string;
+}
+
+interface IOther {
+  'official-artwork': IFront;
+}
+
+interface IPokemonImage {
+  other: IOther;
+}
+
+interface IPokemonStats {
+  base_stat: number;
+  stat: IPokeApi;
+}
+
+export interface IPokemonDetails {
+  name: string;
+  id: number;
+  sprites: IPokemonImage;
+  types: IPokemonType[];
+  stats: IPokemonStats[];
 }
